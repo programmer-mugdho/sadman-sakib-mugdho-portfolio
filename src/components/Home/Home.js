f8e6844d6c429d02b0f79ee9bb9a3414926427c4
programmer-mugdho <programmer.mugdho@gmail.com>
2020-10-30 21:48:21 +0600

    Animation Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,10 +6,32 @@ import Skills from '../Skills/Skills';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
 import Blog from '../Blog/Blog';
+import Particles from 'react-particles-js';
 
 const Home = () => {
     return (
         <div class='container'>
+            <Particles
+                style={{ position: 'absolute', top: 0, left: 0 }}
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 5
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }}
+            />
             <Navbar />
             <Header />
             <About />
